b9aabe89cc1a07af85ff8c015dffe9521b6f25b8
Tianyu Ge <tianyu@vt.edu>
2019-05-17 16:01:54 -0400

    Setup fullscreen, kiosk mode and on-screen keyboard

---
+++ utils/keybindings.js
@@ -0,0 +1,11 @@
+const remote = require("electron").remote;
+
+document.addEventListener("keydown", event => {
+
+    switch (event.key) {
+        case "F11":
+            console.log("F11 pressed.");
+            remote.getCurrentWindow().maximize()
+            break;
+         }
+});
