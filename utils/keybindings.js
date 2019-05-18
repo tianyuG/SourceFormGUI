3d3f4f45a4899026380bed34c5137b0db38cd377
Tianyu Ge <tianyu@vt.edu>
2019-05-18 15:16:57 -0400

    Stage new files

--- utils/keybindings.js
+++ utils/keybindings.js
@@ -2,10 +2,10 @@ const remote = require("electron").remote;
 
 document.addEventListener("keydown", event => {
 
-    switch (event.key) {
-        case "F11":
-            console.log("F11 pressed.");
-            remote.getCurrentWindow().maximize()
-            break;
-         }
+  switch (event.key) {
+    case "F11":
+      console.log("F11 pressed.");
+      remote.getCurrentWindow().maximize()
+      break;
+  }
 });
---
+++ utils/searchfieldHandler.js
@@ -0,0 +1,16 @@
+// Load keyboard
+var keyboard = $('input:text').keyboard();
+
+// Remove default content in the searchfield textbox on click
+function checkSearchFieldOnClick() {
+    if (document.getElementById("searchfield").value === "find an object to print...") {
+        document.getElementById("searchfield").value = "";
+    }
+}
+
+// Restore default content in the searchfield textbox if textbox loose focus and is blank
+function checkSearchFieldOnBlur() {
+    if (document.getElementById("searchfield").value === "") {
+        document.getElementById("searchfield").value = "find an object to print...";
+    }
+}
