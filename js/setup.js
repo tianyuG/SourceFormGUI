38e04c23684dd896878da2caa096c2979cfdd1d9
Tianyu Ge <tianyu@vt.edu>
2019-05-18 16:36:01 -0400

    Start working on the setup screen

---
+++ js/setup.js
@@ -0,0 +1,34 @@
+// Initial setup for assigning contents on both displays
+
+const remote = require('electron')
+const { app, BrowserWindow, shell } = remote
+const os = require('os')
+const fs = require('electron').remote.require('fs')
+
+let win
+
+function launchAHK() {
+
+}
+
+app.on('ready', () => {
+  fs.access(`C:\\Program Files\\AutoHotkey\\AutoHotkey.exe`, (err) => {
+    if (err) {
+      var node = document.createElement("p")
+      var text = document.createTextNode("AHK is not installed.")
+      node.appendChild(text)
+      document.getElementById('ahk-installation-status').appendChild(node)
+    } else {
+      var button = document.createElement("button")
+      button.innerHTML = "Launch AHK Script"
+      button.onclick = "launchAHK()"
+      document.getElementById('ahk-installation-status').appendChild(button)
+    }
+  })
+  let displays = electron.screen.getAllDisplays()
+  const { d0width, d0height } = electron.screen.getPrimaryDisplay().workAreaSize
+  let d1 = displays.find((display) => {
+    return display.bounds.x !== 0 || display.bounds.y !== 0
+  })
+  const { d1width, d1height } = d1.workAreaSize()
+})
