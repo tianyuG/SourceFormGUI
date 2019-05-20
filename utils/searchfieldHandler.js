5864f9c2ed14485b6d4372cce25bc4b9b3c6091b
Tianyu Ge <tianyu@vt.edu>
2019-05-20 14:50:52 -0400

    Updated keyboard settings

--- utils/searchfieldHandler.js
+++ utils/searchfieldHandler.js
@@ -1,13 +1,17 @@
 // Remove default content in the searchfield textbox on click
 function checkSearchFieldOnClick() {
-    if (document.getElementById("searchfield").value === "find an object to print...") {
-        document.getElementById("searchfield").value = "";
-    }
+  if (document.getElementById("searchfield").value === "find an object to print...") {
+    document.getElementById("searchfield").value = "";
+  }
 }
 
 // Restore default content in the searchfield textbox if textbox loose focus and is blank
 function checkSearchFieldOnBlur() {
-    if (document.getElementById("searchfield").value === "") {
-        document.getElementById("searchfield").value = "find an object to print...";
-    }
+  if (document.getElementById("searchfield").value === "") {
+    document.getElementById("searchfield").value = "find an object to print...";
+  }
+}
+
+function checkSearchFieldOnSelect() {
+  checkSearchFieldOnClick();
 }
