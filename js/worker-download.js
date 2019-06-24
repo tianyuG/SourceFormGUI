32652f3e23ec410826ab814b06af742d95ecedeb
Tianyu Ge <tianyu@vt.edu>
2019-06-24 15:54:58 -0400

    Can now populate manifest

--- js/worker-download.js
+++ js/worker-download.js
@@ -54,12 +54,10 @@ ipcRenderer.on('worker-download-search', (event, message) => {
 	let currIndex = (parseInt(lastIndex, 10) + 1)
 		.toString()
 		.padStart(4, '0')
-	let currRecord = "{ \"title\": \"" + name_s + "\", \"sanitised_title\": \"" + name_f + "\", \"path\": \"" + absImagePath.replace('\\', "\\\\") + "\", \"thumbnail\": \"" + currIndex + ".jpg\", \"updated\": \"" + t_iso + "\", \"accessed\": \"" + t_iso + "\", \"image_count\": " + currImageCount + ", \"display\": false }"
+	let currRecord = "{ \"title\": \"" + name_s + "\", \"sanitised_title\": \"" + name_f + "\", \"path\": \"" + absImagePath.replace(/\\+/g, "\\\\") + "\", \"thumbnail\": \"" + currIndex + ".jpg\", \"updated\": \"" + t_iso + "\", \"accessed\": \"" + t_iso + "\", \"image_count\": " + currImageCount + ", \"display\": false }"
 	logMain(currRecord)
-	// jsonObj[currIndex] = JSON.parse(currRecord)
-	jsonObj[currIndex] = currRecord
-
-	// logMain(JSON.stringify(jsonObj))
+	jsonObj[currIndex] = JSON.parse(currRecord)
+	// jsonObj[currIndex] = currRecord
 
 	fs.writeFile(path.resolve(__dirname, "../carousel/content.json"), JSON.stringify(jsonObj, null, 2), (err) => {
 		if (err) {
@@ -69,15 +67,11 @@ ipcRenderer.on('worker-download-search', (event, message) => {
 
 	// Search for images
 	var results = []
-	var promises = []
 	var resultsCount = 0
 	var perPage = require('electron')
 		.remote.getGlobal('imagesPerPage')
-	for (var i = 0; i < currImageCount + perPage; i += perPage) {
-		logMain("// " + i + " " + parseInt(i / 100))
-		// var tst = test(name_s, perPage, i % 100)
-		// logDebug(tst.toString())
-		// promises.push(tst)
+	var pagesNeeded = parseInt(currImageCount / perPage) + 2
+	for (var i = 0; i < pagesNeeded; i++) {
 		var result = flickr.photos.search({
 				text: name_s,
 				extras: "url_n, owner_name, description, license",
@@ -85,115 +79,25 @@ ipcRenderer.on('worker-download-search', (event, message) => {
 				safe_search: 3,
 				sort: "relevance",
 				per_page: perPage,
-				page: parseInt(i / 100)
+				page: i
 			})
-			.then(writeTo(i))
-			// .then(function(res) {
-			// 	results.push(res)
-			// })
+			.then(function(res) {
+				resultsCount += Object.keys(res.body.photos.photo)
+					.length
+				logDebug(JSON.stringify(res.body))
+				logDebug("[WK_DLD] number of images downloaded: " + resultsCount)
+				pageIndex = res.body.photos.page
+				var manifestRelPath = "./manifest_" + pageIndex + ".json"
+				logMain(manifestRelPath)
+				fs.writeFile(path.resolve(absImagePath, manifestRelPath), JSON.stringify(res.body.photos, null, 2), (err) => {
+					if (err) {
+						logMain("[WK_DLD] Could not update manifest: " + err)
+					}
+				})
+			})
 			.catch(function(err) {
 				logMain("[WK_DLD] image download failed: " + err);
 			});
-
-		// 	})
-		// 	.then(function(res) {
-		// 		// logDebug(JSON.stringify(res.body.photos))
-		// 		// results.push(JSON.stringify(res.body.photos))
-		// 	})
-		// .then(function(res) {
-		// 	// logDebug(JSON.stringify(res.body.photos))
-		// 	logMain((i)
-		// 		.toString() + "   " + (i / 100)
-		// 		.toString())
-		// 	resultsCount += Object.keys(res.body.photos.photo)
-		// 		.length
-		// 	logDebug("[WK_DLD] number of images downloaded:" + resultsCount)
-		// 	var manifestRelPath = "./manifest_" + (i / 100)
-		// 		.toString() + ".json"
-		// 	logMain(manifestRelPath)
-		// 	fs.writeFile(path.resolve(absImagePath, manifestRelPath), JSON.stringify(res.body.photos, null, 2), (err) => {
-		// 		if (err) {
-		// 			logMain("[WK_DLD] Could not update manifest: " + err)
-		// 		}
-		// 	})
-		// })
-		// .catch(function(err) {
-		// 	logMain("[WK_DLD] image download failed: " + err);
-		// });
-		// results.push(result)
-		// logMain(JSON.stringify(results))
 	}
 
-	logMain(JSON.stringify(promises))
-
-	// Promise.all(promises)
-	// 	.then(function(res) {
-	// 		logDebug(JSON.stringify(res.body.photos))
-	// 		logMain((i)
-	// 			.toString() + "   " + (i / 100)
-	// 			.toString())
-	// 		resultsCount += Object.keys(res.body.photos.photo)
-	// 			.length
-	// 		logDebug("[WK_DLD] number of images downloaded:" + resultsCount)
-	// 		var manifestRelPath = "./manifest_" + (i / 100)
-	// 			.toString() + ".json"
-	// 		logMain(manifestRelPath)
-	// 		fs.writeFile(path.resolve(absImagePath, manifestRelPath), JSON.stringify(res.body.photos, null, 2), (err) => {
-	// 			if (err) {
-	// 				logMain("[WK_DLD] Could not update manifest: " + err)
-	// 			}
-	// 		})
-
-	// 	})
-	// 	.catch(function(err) {
-	// 		if (err) {
-	// 			logMain("[WK_DLD] image download failed: " + err);
-	// 		}
-	// 	})
-
-	// Download images
-
-	searchFor(name_s, "url_l", remote.getGlobal('numberOfImagesPerModel'))
-
 })
-
-/*
- * Search for the requested term and returns a json object with urls (among other things)
- * NOTE: This function expects a *sanitised* input
- * size (str): "url_sq", "url_s", "url_q", "url_t", "url_m", "url_n", "url_z", 
- * 						 "url_c", "url_l", "url_o"
- */
-function searchFor(term, size, numOfImgs) {
-
-}
-
-// function processManifest()
-// 
-const test = async (message, perPage, pageid) => {
-	await flickr.photos.search({
-		text: message,
-		extras: "url_n, owner_name, description, license",
-		privacy_filter: 1,
-		safe_search: 3,
-		sort: "relevance",
-		per_page: perPage,
-		page: pageid
-	})
-}
-
-async function writeTo(i) {
-	// logDebug(JSON.stringify(res.body.photos))
-	logMain((i)
-		.toString() + "   " + (i / 100)
-		.toString())
-	resultsCount += Object.keys(res.body.photos.photo)
-		.length
-	var manifestRelPath = "./manifest_" + (i / 100)
-		.toString() + ".json"
-	logMain(manifestRelPath)
-	fs.writeFile(path.resolve(absImagePath, manifestRelPath), JSON.stringify(res.body.photos, null, 2), (err) => {
-		if (err) {
-			logMain("[WK_DLD] Could not update manifest: " + err)
-		}
-	})
-}
