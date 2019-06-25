bb543db9d1c3c4c1826961a56361143d66bf08a7
Tianyu Ge <tianyu@vt.edu>
2019-06-25 14:53:44 -0400

    Fixed problems with populating manifest

--- js/worker-download.js
+++ js/worker-download.js
@@ -18,7 +18,7 @@ var fs = require('graceful-fs')
 var flickr = new Flickr(require('electron')
 	.remote.getGlobal('flickrKey'))
 
-ipcRenderer.on('worker-download-search', (event, message) => {
+ipcRenderer.on('worker-download-search', async (event, message) => {
 	var t = new Date()
 	var t_iso = t.toISOString()
 	var name_o = message.trim()
@@ -39,9 +39,9 @@ ipcRenderer.on('worker-download-search', (event, message) => {
 	// Creating folder
 	fs.mkdir(absImagePath, (err) => {
 		if (err) {
-			logMain("[WK_DLD] " + absImagePath + " creation failed: " + err)
+			logMain("[WK_DLD] failed to create folder " + absImagePath + ": " + err)
 		} else {
-			logMain("[WK_DLD] created: " + absImagePath)
+			logMain("[WK_DLD] created folder: " + absImagePath)
 		}
 	})
 
@@ -61,20 +61,66 @@ ipcRenderer.on('worker-download-search', (event, message) => {
 
 	fs.writeFile(path.resolve(__dirname, "../carousel/content.json"), JSON.stringify(jsonObj, null, 2), (err) => {
 		if (err) {
-			logMain("[WK_DLD] Could not update catalogue: " + err)
+			logMain("[WK_DLD] Could not update carousel content: " + err)
 		}
 	});
 
-	// Search for images
+	// Populate manifest with search results
 	var results = []
-	var resultsCount = 0
+	var imageSize = "url_n"
 	var perPage = require('electron')
 		.remote.getGlobal('imagesPerPage')
+	await populateManifest(currImageCount, perPage, name_s, imageSize, absImagePath)
+	// loadImageURLs(currImageCount, perPage, absImagePath)
+	
+	// var resultsCount = 0
+	// var perPage = require('electron')
+	// 	.remote.getGlobal('imagesPerPage')
+	// var pagesNeeded = parseInt(currImageCount / perPage) + 2
+	// var imageSize = "url_n"
+	// for (var i = 1; i < pagesNeeded; i++) {
+	// 	var result = flickr.photos.search({
+	// 			text: name_s,
+	// 			extras: imageSize + ", owner_name, description, license",
+	// 			privacy_filter: 1,
+	// 			safe_search: 3,
+	// 			sort: "relevance",
+	// 			per_page: perPage,
+	// 			page: i
+	// 		})
+	// 		.then(function(res) {
+	// 			resultsCount += Object.keys(res.body.photos.photo)
+	// 				.length
+	// 			// logDebug(JSON.stringify(res.body))
+	// 			logDebug("[WK_DLD] images in manifests: " + resultsCount)
+	// 			pageIndex = res.body.photos.page
+	// 			var manifestRelPath = "./manifest_" + pageIndex + ".json"
+	// 			logMain(manifestRelPath)
+	// 			fs.writeFile(path.resolve(absImagePath, manifestRelPath), JSON.stringify(res.body.photos, null, 2), (err) => {
+	// 				if (err) {
+	// 					logMain("[WK_DLD] failed to populate manifest: " + err)
+	// 				} else {
+	// 					results.push(res.body.photos.photo)
+	// 				}
+	// 			})
+	// 		})
+	// 		.catch(function(err) {
+	// 			logMain("[WK_DLD] failed to populate manifest: " + err);
+	// 		});
+
+	// 		logDebug("RES   " + results)
+	// }
+
+})
+
+const populateManifest = async (currImageCount, perPage, name_s, imageSize, absImagePath, callback) => {
+	var resultsCount = 0
 	var pagesNeeded = parseInt(currImageCount / perPage) + 2
-	for (var i = 0; i < pagesNeeded; i++) {
+
+	for (var i = 1; i < pagesNeeded; i++) {
 		var result = flickr.photos.search({
 				text: name_s,
-				extras: "url_n, owner_name, description, license",
+				extras: imageSize + ", owner_name, description, license",
 				privacy_filter: 1,
 				safe_search: 3,
 				sort: "relevance",
@@ -84,20 +130,36 @@ ipcRenderer.on('worker-download-search', (event, message) => {
 			.then(function(res) {
 				resultsCount += Object.keys(res.body.photos.photo)
 					.length
-				logDebug(JSON.stringify(res.body))
-				logDebug("[WK_DLD] number of images downloaded: " + resultsCount)
+				// logDebug(JSON.stringify(res.body))
+				logDebug("[WK_DLD] images in manifests: " + resultsCount)
 				pageIndex = res.body.photos.page
 				var manifestRelPath = "./manifest_" + pageIndex + ".json"
-				logMain(manifestRelPath)
+				// logMain(manifestRelPath)
 				fs.writeFile(path.resolve(absImagePath, manifestRelPath), JSON.stringify(res.body.photos, null, 2), (err) => {
 					if (err) {
-						logMain("[WK_DLD] Could not update manifest: " + err)
+						logMain("[WK_DLD] failed to populate manifest: " + err)
+					} else {
+						// results.push(res.body.photos.photo)
 					}
 				})
 			})
 			.catch(function(err) {
-				logMain("[WK_DLD] image download failed: " + err);
+				logMain("[WK_DLD] failed to populate manifest: " + err);
 			});
+		// logDebug("responses   " + results)
 	}
+}
 
-})
+const loadImageURLs = async (currImageCount, perPage, absImagePath) => {
+	var pagesNeeded = parseInt(currImageCount / perPage) + 2
+	for (var i = 1; i < pagesNeeded; i++) {
+		var manifestRelPath = "./manifest_" + i + ".json"
+		await fs.readFile(path.resolve(absImagePath, manifestRelPath), (err, data) => {
+			if (err) {
+				logDebug("[WK_DLD] failed to load image url: " + err)
+			} else {
+				logDebug(data)
+			}
+		})
+	}
+}
