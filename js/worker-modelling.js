e501efcd292caca05cd6a267c3efd787a3583476
Tianyu Ge <tianyu@vt.edu>
2019-07-11 10:43:48 -0400

    Updated exec

--- js/worker-modelling.js
+++ js/worker-modelling.js
@@ -14,28 +14,42 @@ ipcRenderer.on('worker-modelling-request', async (event, message) => {
 	let projName = message['projname']
 	let absPath = message['abspath']
 	let rmtPath = message['rmtpath']
+	let cmds = message['commands']
 
 	let Client = ssh.Client()
 	let conn = new Client()
 
 	conn.on('ready', () => {
-			conn.exec(path.join(rmtPath, "run.bat").replace(/\\+/g, '\\\\'), (err, stream) => {
-				if (err) {
-					logMain("[WK_MDL] ssh2 - COLMAP failed: " + err)
-				}
+			// conn.exec(path.join(rmtPath, "run.bat").replace(/\\+/g, '\\\\'), (err, stream) => {
+			// 	if (err) {
+			// 		logMain("[WK_MDL] ssh2 - COLMAP failed: " + err)
+			// 	}
 
-				stream.on('close', (ecode, signal) => {
-						logMain("[WK_MDL] ssh2 - COLMAP stream close: exit code " + ecode + ", signal: " + signal)
-						// Retrieve bmps once it's complete
-						conn.end()
-					})
-					.on('data', (data) => {
-						// STDOUT
-						// Monitor stdout and retrieve ply model once it's complete
-					})
-					.stderr.on('data', (err) => {
-						// STDERR
-					})
+			// 	stream.on('close', (ecode, signal) => {
+			// 			logMain("[WK_MDL] ssh2 - COLMAP stream close: exit code " + ecode + ", signal: " + signal)
+			// 			// Retrieve bmps once it's complete
+			// 			conn.end()
+			// 		})
+			// 		.on('data', (data) => {
+			// 			// STDOUT
+			// 			// Monitor stdout and retrieve ply model once it's complete
+			// 		})
+			// 		.stderr.on('data', (err) => {
+			// 			// STDERR
+			// 		})
+			// })
+			conn.exec(cmds[0], (err, stream) => {
+				if (err) { logMain("[WK_MDL] ssh2 - exec/0 failed: " + err) }
+			})
+
+			stream.on('close', (ecode, sig) => {
+				logMain("[WK_MDL] ssh2 - exec/0 stream close: exit code " + ecode + ", signal: " + signal)
+				// exec/1
+				// 
+			}).on('data', (data) => {
+				// stdout
+			}).stderr.on('data', (data) => {
+				// stderr
 			})
 		})
 		.connect({
