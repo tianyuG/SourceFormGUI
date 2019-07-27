ec67155ec70a8e68d9bce31b9eb91a2ea08f5091
Tianyu Ge <tianyu@vt.edu>
2019-07-27 18:09:11 -0400

    Adding interface with the printer

--- js/worker-printing.js
+++ js/worker-printing.js
@@ -2,10 +2,23 @@ const {
 	ipcRenderer,
 	remote
 } = require('electron')
-const cproc = require('child_process')
+const { execFile } = require('child_process')
 
 ipcRenderer.on('worker-printing-request', async (event, message) => {
 	// TODO: Might look into npm package johnny-five
 	// cproc.exec()
 	// python [printer_script] [path_to_image_slices]
+	execFile('python', [message[localSlicesPath]], (err, stdout, stderr) => {
+		if (error) {
+			logMain("[WK_PRN] failed to execFile: " + error)
+		}
+
+		if (stdout) {
+			logMain("[WK_PRN] execFile STDOUT: " + stdout)
+		}
+
+		if (stderr) {
+			logMain("[WK_PRN] execFile STDERR: " + stderr)
+		}
+	})
 })
